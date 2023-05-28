83bd60ff09c6c8a8485ec654a1f5099e523344e8
monsieurmusclaira <hello@victormaes.com>
2023-05-28 12:14:55 +0200

    Migration away from partytown
    Changing to astro-google-analytics integration for GA4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image({serviceEntryPoint: '@astrojs/image/sharp'}), mdx(),
-  partytown({
-    // Adds dataLayer.push as a forwarding-event.
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }),
-], 
+  integrations: [tailwind(), sitemap(), image({serviceEntryPoint: '@astrojs/image/sharp'}), mdx()], 
   markdown: {
   },
 });
