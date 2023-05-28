a2fcecc32e67def381d0e8a4ea0904afa92f254a
monsieurmusclaira <hello@victormaes.com>
2023-05-28 11:50:25 +0200

    Google Analytics bug fixing
    Seems I'm not receiving any data. It might because of using the wrong quotation marks.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
 import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), image({serviceEntryPoint: '@astrojs/image/sharp'}), mdx(),
