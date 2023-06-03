675fd166b35c7e51885be0cc4d78e3471b8e7060
monsieurmusclaira <hello@victormaes.com>
2023-06-03 18:55:26 +0200

    v2.1.4 - add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import { astroImageTools } from "astro-imagetools";
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://monsieurmusclaira.github.io',
-  integrations: [
-    tailwind(), 
-    sitemap(), 
-    image({serviceEntryPoint: '@astrojs/image/sharp'}), 
-    astroImageTools,
-    mdx(),
-  ], 
-  markdown: {
-  },
+  integrations: [tailwind(), sitemap(), astroImageTools, mdx()],
+  markdown: {},
+  output: "server",
+  adapter: netlify()
 });
