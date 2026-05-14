0f335463e72091b06aa82231f442183cb83908e0
monsieurmusclaira <maes.victor.1@gmail.com>
2026-05-14 09:42:57 +0200

    v 3.0.5 - SEO update
    Big SEO update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Victor Maes - Cinematography portfolio';
-export const SITE_DESCRIPTION = 'Victor Maes is a Cinematographer, Director of Photography and Director from Brussels Belgium';
+export const SITE_TITLE = 'Victor Maes — Cinematographer & Director';
+export const SITE_DESCRIPTION = 'Victor Maes is an award-winning cinematographer, director of photography and director based in Brussels, Belgium. Portfolio of fiction, documentary, VR and experimental films.';
+export const SITE_URL = 'https://victormaes.com';
